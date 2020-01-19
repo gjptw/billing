15221c148e56222d0cf1a6c76b55d6d57b8b6204
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 22:33:14 +0300

    Fix xml

--- src/main/java/com/company/billing/Application.java
+++ src/main/java/com/company/billing/Application.java
@@ -9,12 +9,12 @@ import java.util.Arrays;
 //@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+       // ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
 //        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
 //        obj.printHello();
 
-       System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+       //System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
       // ClientRepository clientRepository = (ClientRepository)context.getBean("clientRepository");
       // System.out.println(clientRepository.findAll());
