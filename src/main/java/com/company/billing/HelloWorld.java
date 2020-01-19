2dab9fa4b740491e2fc737ab7ca2b6be24c1d2d8
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 17:08:49 +0300

    Initial

---
+++ src/main/java/com/company/billing/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company.billing;
+
+public class HelloWorld {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Spring 5 : Hello ! " + name);
+    }
+}
