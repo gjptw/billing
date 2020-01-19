920f09e4b24d1ce14d3cfe1a78698f2bb56fdbf5
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 23:04:37 +0300

    Fix context

--- src/main/java/com/company/billing/Application.java
+++ src/main/java/com/company/billing/Application.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 //@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
+
+        new SpringApplication(AppContext.class).run(args);
+
         // ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
 //        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
