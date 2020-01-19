c0bdf8234879a219467e2daa587eff9d8bc6e3aa
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 19:32:19 +0300

    Add repositories

--- src/main/java/com/company/billing/Application.java
+++ src/main/java/com/company/billing/Application.java
@@ -1,13 +1,24 @@
 package com.company.billing;
 
+import com.company.billing.jpa.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Arrays;
+
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
 
-        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
-        obj.printHello();
+//        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+//        obj.printHello();
+
+       System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+
+       ClientRepository clientRepository = (ClientRepository)context.getBean("clientRepository");
+       System.out.println(clientRepository.findAll());
     }
 }
--- src/main/java/com/company/billing/jpa/ClientRepository.java
+++ src/main/java/com/company/billing/jpa/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.company.billing.jpa;
 
 import com.company.billing.model.Client;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository("clientRepository")
 public interface ClientRepository extends BaseRepository<Client, Long> {
 
         List<Client> findAll();
