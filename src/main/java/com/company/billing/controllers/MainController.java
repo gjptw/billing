e94c95b5beafb24122d85f33d1f502efccd46823
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 23:44:37 +0300

    Add controller

--- src/main/java/com/company/billing/controllers/MainController.java
+++ src/main/java/com/company/billing/controllers/MainController.java
@@ -3,10 +3,13 @@ package com.company.billing.controllers;
 import com.company.billing.model.Client;
 import com.company.billing.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -14,8 +17,14 @@ public class MainController {
     private ClientRepository clientRepository;
 
     @RequestMapping("/all")
-    public List<Client> ivanAction() {
-        return clientRepository.findAll();
+    public ResponseEntity<List<Client>> ivanAction() {
+        return entityWithContent(clientRepository.findAll());
+    }
+
+    private static <T> ResponseEntity<T> entityWithContent(T t) {
+        return Optional.ofNullable(t)
+                .map(content -> ResponseEntity.status(HttpStatus.OK).body(content))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 }
