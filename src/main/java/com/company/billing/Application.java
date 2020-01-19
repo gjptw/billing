0560dd0c81c849ce2599837da15e98f62c991248
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 17:50:41 +0300

    Create sql

--- src/main/java/com/company/billing/Application.java
+++ src/main/java/com/company/billing/Application.java
@@ -5,8 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                "SpringBeans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
 
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
