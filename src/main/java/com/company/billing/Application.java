bf36e044328d6c1dbd61f680cbedf787abe7b5d7
Ihar Sopat <iharsopat@ya.ru>
2020-01-19 23:07:27 +0300

    Fix context

--- src/main/java/com/company/billing/Application.java
+++ src/main/java/com/company/billing/Application.java
@@ -10,12 +10,7 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
 
-        new SpringApplication(AppContext.class).run(args);
-
         // ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-
-//        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
-//        obj.printHello();
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AppContext.class);
         Client client = context.getBean(Client.class);
